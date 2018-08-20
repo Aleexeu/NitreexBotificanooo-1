37f8f278bdcf3345f7d12fe8dfe944cc62cb4afe
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-08-20 13:28:13 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('message', message => {
         message.channel.send('Em desenvolvimento!');
     }
     f (message.content.startsWith('/ip')){
-        message.channel.send('ghostminigames.  \ncomghostsurvival.com');
+        message.channel.send('ghostminigames.\ncomghostsurvival.com.');
     }
     let arraymsg = message.content.split(" ");
 let cmd = arraymsg[0].toLowerCase()
