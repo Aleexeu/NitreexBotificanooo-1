62e266f3d72e808565f4bdad732c247e2243357a
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-08-21 22:54:04 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,20 @@ const client = new Discord.Client();
 
 
 client.on('ready', () => {
-    client.user.setPresence({ game: { name: `/comandos`, type: 1, url: 'https://www.youtube.com/yRecky'} });
     console.log('Logado');
+});
+client.on('message', message => {
+    let arraymsg = message.content.split(" ");
+let cmd = arraymsg[0].toLowerCase()
+let args = message.content.split(" ").slice(1);
+    if(cmd === '/anuncio'){
+    if(!message.member.hasPermission("MANAGE_ROLES")) return;
+       const sayMessage = args.join(" ");
+    message.delete()
+       const embed = new Discord.RichEmbed()
+       .setDescription(sayMessage)
+       message.channel.send(embed);
+    }
 });
 client.on('message', message => {
     let arraymsg = message.content.split(" ");
