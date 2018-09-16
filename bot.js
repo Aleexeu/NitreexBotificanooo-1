91b0fe65790d205711a882e665882ea83268b55e
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-15 22:31:50 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -231,10 +231,10 @@ let cmd = arraymsg[0].toLowerCase()
 let args = message.content.split(" ").slice(1)
     if(cmd === '/sugestao'){
         exports.run = (client,message,args)=>{
-    var sugest = args.slice(0).join(" ")
+    var sugest = args.slice(0).join(" ");
     if(!sugest) return message.reply("escreva sua sugestão")
-        client.guilds.get("406209503684722689").channels.get("433308958644633611").send("----------------------Sugestão------------------------\n"+ sugest + "\n sugestão enviada por <@" + message.author.id + "> ou " + message.author.tag + "\n---------------------fim--------------------")
-    message.channel.send("sua sugestão foi enviada para meu server")
+        client.guilds.get("406209503684722689").channels.get("433308958644633611").send("----------------------Sugestão------------------------\n"+ sugest + "\n sugestão enviada por <@" + message.author.id + "> ou " + message.author.tag + "\n---------------------fim--------------------");
+    message.channel.send("sua sugestão foi enviada para meu server");
 }
 
 });
