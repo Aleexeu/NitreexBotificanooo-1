1c209f20a18477c68c7ebc5d83c13a7d31375871
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-21 12:00:37 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ const bot = new Discord.Client();
 
 bot.on('ready', () => {
     let status = [
-        {name: 'comandos?│/comandos', type: 'STREAMING', url: 'https://twitch.tv/srmisterii'},
+        {name: 'comandos?│/comandos', type: 'STREAMING', url: 'https://twitch.tv/nitroopvp'},
         {name: 'CrownMC.reis.host', type: 'STREAMING', url: 'https://twitch.tv/srmisterii'},
-        {name: 'Fui desenvolvido pelo NitrooPVP#4025', type: 'STREAMING', url: 'https://twitch.tv/srmisterii'},
+        {name: 'Fui desenvolvido pelo NitrooPVP#4025', type: 'STREAMING', url: 'https://twitch.tv/srmiterii'},
       ];
       
       //STREAMING = Transmitindo
