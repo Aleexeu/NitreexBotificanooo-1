85ee023148ff431db8f35a333f3af3c7599ab615
vitorhugo643 <42257427+vitorhugo643@users.noreply.github.com>
2018-09-21 07:09:20 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 
 bot.on('ready', () => {
     let status = [
-        {name: 'comandos?│/comandos, type: 'STREAMING', url: 'https://twitch.tv/srmisterii'},
+        {name: 'comandos?│/comandos', type: 'STREAMING', url: 'https://twitch.tv/srmisterii'},
         {name: 'test', type: 'STREAMING', url: 'https://twitch.tv/srmisterii'},
       ];
       
